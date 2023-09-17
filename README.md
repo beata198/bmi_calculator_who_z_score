c0999934698d17012d224f4ec814e20ca82cad0b
beata198 <b.lublewska@gmail.com>
2023-09-17 14:35:48 +0200

    Add BMI Calculator for Adults and Children

---
+++ README.md
@@ -0,0 +1,8 @@
+<!-- Aplikacja, o której mowa, jest innowacyjnym narzędziem mającym na celu ułatwienie korzystania z kalkulatora wskaźnika masy ciała (BMI) dla różnych grup wiekowych. Dzięki temu narzędziu użytkownicy mogą szybko i wygodnie obliczać wskaźnik BMI dla dorosłych oraz dzieci, wybierając odpowiednią grupę wiekową. Aplikacja dostarcza interaktywny interfejs, w którym użytkownik może wybrać grupę wiekową, a następnie wprowadzić wymagane dane, takie jak masa ciała i wzrost. Na podstawie tych informacji, aplikacja dokonuje precyzyjnych obliczeń i prezentuje użytkownikowi wartość BMI wraz z interpretacją wyniku, która pomaga zrozumieć, czy wskaźnik mieści się w granicach normy zdrowotnej. Ta aplikacja stanowi praktyczne narzędzie dla osób dbających o zdrowie oraz specjalistów medycznych, umożliwiając szybkie monitorowanie stanu zdrowia i kondycji fizycznej zarówno u dorosłych, jak i u dzieci. -->
+
+<!--
+Dla dorosłych, użytkownicy
+          mogą wprowadzić swoją wagę i wzrost, a następnie aplikacja oblicza ich
+          BMI. W oparciu o to BMI, aplikacja dostarcza informacje na temat
+          zakresów, takich jak niedowaga, prawidłowa masa ciała, nadwaga i różne
+          poziomy otyłości. <br /><br /> -->
--- script.js
+++ script.js
@@ -1,157 +1,389 @@
 "use strict";
 
-const [male, female] = document.getElementsByName("gender");
+const radioButtons = document.querySelectorAll('input[name="info"]');
 
-// console.log(male.checked, female.checked);
+const BMIFormAdult = document.getElementById("bmi-adult");
+const BMIFormChild = document.getElementById("bmi-child");
 
-const inputHeight = document.getElementById("height__input");
-const outputHeight = document.querySelector(".height__input--output");
+let heightValue,
+  weightValue,
+  heightOutputValue,
+  submit,
+  btnDecrease,
+  btnIncrease,
+  weightMeasure;
 
-const inputWeight = document.getElementById("weight__input");
-const inputAge = document.getElementById("age__input");
+// function for setting initial values
+const setInitialValues = (
+  heightInputId,
+  weightInputId,
+  heightOutputSelector,
+  btnId,
+  btnD,
+  btnI,
+  weightM
+) => {
+  heightValue = document.getElementById(heightInputId);
+  weightValue = document.getElementById(weightInputId);
+  heightOutputValue = document.querySelector(heightOutputSelector);
 
-//buttons
-const submit = document.querySelector(".btn");
-const btnMinusWeight = document.querySelector(".weightDown");
-const btnPlusWeight = document.querySelector(".weightUp");
-const btnMinusAge = document.querySelector(".ageDown");
-const btnPlusAge = document.querySelector(".ageUp");
+  heightValue.value = 150;
+  weightValue.value = 0;
 
-//default values
-inputHeight.value = outputHeight.textContent = 150;
-inputAge.value = inputWeight.value = 0;
-const lb = 0.45359237;
+  heightOutputValue.textContent = `150 cm / 4' 11''`;
 
-//display the current selected value - height
-inputHeight.oninput = () => {
-  outputHeight.textContent = `${inputHeight.value}`;
+  heightValue.oninput = () => {
+    heightOutputValue.textContent = `${heightValue.value} cm / ${calcInAndFt(
+      +heightValue.value
+    )}`;
+  };
+  submit = document.querySelector(btnId);
+  btnDecrease = document.querySelector(btnD);
+  btnIncrease = document.querySelector(btnI);
+  decrease(weightValue, btnDecrease);
+  increase(weightValue, btnIncrease, btnDecrease);
+  //listening to see if the user entered <= 0 -- inputWeight
+  listenerForEnteredZero(weightValue, btnDecrease);
+  weightMeasure = document.getElementById(weightM);
 };
 
-//btn addition, subtraction - weight, age
-function calc(button, v, chart) {
-  button.addEventListener("click", (e) => {
+// function to convert in and ft from cm
+const calcInAndFt = (v) => {
+  const i = Math.floor(v / 30.48);
+  const f = Math.floor((v - i * 30.48) * 0.393701);
+  return `${i}' ${f}''`;
+};
+
+// function decrease, increase
+const decrease = (input, btn) => {
+  btn.addEventListener("click", (e) => {
     e.preventDefault();
-    if (+v.value >= 0 && chart === -1) {
-      v.value = v.value - chart;
-    } else if (+v.value > 0 && chart === 1) {
-      v.value = v.value - chart;
-    } else {
-      v.value = 0;
+    let newValue = +input.value - 1;
+    if (newValue >= 0) {
+      input.value = newValue;
+      if (newValue === 0) {
+        btn.disabled = true;
+      }
     }
   });
-}
+};
 
-calc(btnPlusAge, inputAge, -1);
-calc(btnMinusAge, inputAge, 1);
-calc(btnPlusWeight, inputWeight, -1);
-calc(btnMinusWeight, inputWeight, 1);
+const increase = (input, btn, btnDecrease) => {
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    input.value = +input.value + 1;
+    if (input.value > 0) {
+      btnDecrease.disabled = false;
+    }
+  });
+};
+
+// function to disable the button when it is 0
+const listenerForEnteredZero = (input, btn) => {
+  input.addEventListener("change", () => {
+    if (+input.value <= 0) {
+      input.value = 0;
+    }
+    btn.disabled = +input.value === 0;
+  });
+};
+
+//change lb/kg
+const lbOnKg = (lb) => {
+  const kg = (lb * 0.45359237).toFixed(2);
+  return kg;
+};
+
+// calc BMI
+const calcBMI = (weight, height) => {
+  const BMI = (weight / (height / 100) ** 2).toFixed(2);
+  let description = "";
+
+  if (BMI <= 18.5) {
+    description = "niedowaga";
+  } else if (BMI <= 24.9) {
+    description = "prawidłowa masa ciała";
+  } else if (BMI <= 29.9) {
+    description = "nadwaga";
+  } else if (BMI <= 34.9) {
+    description = "otyłość I stopnia";
+  } else if (BMI <= 39.9) {
+    description = "otyłość II stopnia";
+  } else if (BMI >= 40) {
+    description = "otyłość III stopnia (otyłość skrajna)";
+  }
+  return { description, BMI };
+};
+
+// change form when user click select
+radioButtons.forEach((radioButton) => {
+  radioButton.addEventListener("change", function () {
+    if (this.value === "adult") {
+      if (!BMIFormChild.classList.contains("hidden")) {
+        BMIFormChild.classList.add("hidden");
+      }
+      BMIFormAdult.classList.remove("hidden");
+
+      setInitialValues(
+        "heightAdult",
+        "weightAdult",
+        ".height__output--adult",
+        ".btn-adult",
+        ".decreaseWeightAdult",
+        ".increaseWeightAdult",
+        "measureWeightAdult"
+      );
+      submit.addEventListener("click", (e) => {
+        e.preventDefault();
+        let resultsDescription = "";
+        const weight =
+          weightMeasure.value === "kg"
+            ? +weightValue.value
+            : lbOnKg(+weightValue.value);
+
+        weightValue.style.border = "1px solid transparent";
+        document.querySelector(".weight__subtitle").style.color = "#fff";
+
+        if (+weightValue.value > 0) {
+          const resultBMI = calcBMI(weight, +heightValue.value);
+          resultsDescription = `Twój wskaźnik BMI wynosi: ${
+            resultBMI.description
+          } ${resultBMI.BMI}\r\n
+          Twój wzrost wynosi ${+heightValue.value}
+          \r\n Twoja waga ${
+            weightMeasure.value === "kg"
+              ? +weightValue.value + " kg"
+              : +weightValue.value + " lb"
+          }`;
+        } else {
+          if (+weightValue.value <= 0) {
+            resultsDescription +=
+              "Uzupełnij formularz! \r\n Waga nie może być równa/mniejsza 0.";
+            weightValue.style.border = "2px solid #E9190F";
+            document.querySelector(".weight__subtitle").style.color = "#E9190F";
+          }
+        }
+
+        resultsDiv.textContent = resultsDescription;
+        resultsDiv.innerHTML = resultsDiv.innerHTML.replace(/\r\n?/g, "<br />");
+        resultsDiv.style.opacity = 1;
+      });
+    } else if (this.value === "child") {
+      if (!BMIFormAdult.classList.contains("hidden")) {
+        BMIFormAdult.classList.add("hidden");
+      }
+      BMIFormChild.classList.remove("hidden");
+
+      setInitialValues(
+        "heightChild",
+        "weightChild",
+        ".height__output--child",
+        ".btn-child",
+        ".decreaseWeightChild",
+        ".increaseWeightChild",
+        "measureWeightChild"
+      );
+
+      resultsDiv.style.opacity = 0;
+
+      const ageDate = document.getElementById("age__input");
+      const measurementDate = document.getElementById("date__input");
+      measurementDate.value = new Date().toISOString().split("T")[0];
+      const currentDate = new Date().toISOString().split("T")[0];
+      ageDate.max = currentDate;
+      measurementDate.max = currentDate;
+
+      submit.addEventListener("click", (e) => {
+        e.preventDefault();
+        let resultsDescription = "";
+        const weight =
+          weightMeasure.value === "kg"
+            ? +weightValue.value
+            : lbOnKg(+weightValue.value);
+
+        const [male, female] = document.getElementsByName("gender");
+
+        const birthday = new Date(ageDate.value);
+
+        const monthDifference = (dateTo, dateFrom) => {
+          const diff =
+            dateTo.getMonth() -
+            dateFrom.getMonth() +
+            12 * (dateTo.getFullYear() - dateFrom.getFullYear());
+          return diff;
+        };
+
+        const months = monthDifference(
+          new Date(measurementDate.value),
+          birthday
+        );
+
+        const genderContent = document.querySelector(".gender__content");
+        const ageSubtitle = document.querySelector(".age__subtitle");
+        const weightSubtitle = document.querySelector(
+          ".weight__subtitle-child"
+        );
+        const genderSubtitle = document.querySelector(".gender__subtitle");
+
+        genderContent.style.border =
+          weightValue.style.border =
+          ageDate.style.border =
+            "1px solid transparent";
+
+        ageSubtitle.style.color =
+          weightSubtitle.style.color =
+          genderSubtitle.style.color =
+            "#fff";
+
+        const fetchDataFromFIle = async () => {
+          let arr = [];
+          if (months > 23 && months < 61) {
+            const res = await fetch(
+              `./assets/data/${male.checked ? "boys" : "girls"}_2_5.json`
+            );
+            const data = await res.json();
+            arr = Object.values(data)[0];
+          } else if (months > 60 && months < 229) {
+            const res = await fetch(
+              `./assets/data/${male.checked ? "boys" : "girls"}_5_19.json`
+            );
+            const data = await res.json();
+            arr = Object.values(data)[0];
+          } else {
+            resultsDescription += "Podaj wiek pomiędzy 2 - 19 lat.";
+          }
+
+          const BMI = (weight / (+heightValue.value / 100) ** 2).toFixed(2);
+          const findMonthValues = arr.find((el) => el.Month === `${months}`);
+
+          const up = Math.pow(+BMI / findMonthValues.M, findMonthValues.L) - 1;
+          const down = findMonthValues.S * findMonthValues.L;
+          const zScore = (up / down).toFixed(2);
+
+          return zScore;
+        };
+
+        if (
+          (male.checked || female.checked) &&
+          months > 23 &&
+          +heightValue.value > 0 &&
+          +weightValue.value > 0 &&
+          (!ageDate.value === "" ||
+            (!isNaN(ageDate.valueAsNumber) &&
+              ageDate.valueAsDate < measurementDate.valueAsDate))
+        ) {
+          const resultBMI = calcBMI(weight, +heightValue.value);
+          fetchDataFromFIle()
+            .then((z) => {
+              resultsDescription = `Twój wskaźnik BMI wynosi: ${
+                resultBMI.description
+              } ${resultBMI.BMI}
+          \r\nTwój wzrost wynosi ${+heightValue.value}
+          \r\n Twoja waga: ${
+            weightMeasure.value === "kg"
+              ? +weightValue.value + " kg"
+              : +weightValue.value + " lb"
+          }
+          \r\n Data pomiaru: ${measurementDate.value}
+          \r\n Data urodzenia: ${ageDate.value}
+          \r\n Z-score: ${z}`;
+              resultsDiv.textContent = resultsDescription;
+              resultsDiv.innerHTML = resultsDiv.innerHTML.replace(
+                /\r\n?/g,
+                "<br />"
+              );
+              resultsDiv.style.opacity = 1;
+            })
+            .catch((er) => {
+              resultsDescription +=
+                "Nie udało pobrać się danych z pliku, do obliczenia z-score... \r\n Spróbuj później";
+            });
+        } else {
+          resultsDescription += "Uzupełnij formularz! \r\n";
+          if (!male.checked && !female.checked) {
+            resultsDescription += "Wybierz płeć dziecka. \r\n";
+            genderContent.style.border = "2px solid #E9190F";
+            genderSubtitle.style.color = "#E9190F";
+          }
+          if (+heightValue.value <= 50) {
+            resultsDescription +=
+              "Wzrost nie może być mniejszy nić 50 cm. \r\n";
+          }
+          if (+weightValue.value <= 0) {
+            resultsDescription += "Waga nie może być równa/mniejsza 0. \r\n";
+            weightValue.style.border = "2px solid #E9190F";
+            weightSubtitle.style.color = "#E9190F";
+          }
+          if (ageDate.value === "" || isNaN(ageDate.valueAsDate)) {
+            resultsDescription += "Wprowadź datę urodzenia dziecka. \r\n";
+            ageDate.style.border = "2px solid #E9190F";
+            ageSubtitle.style.color = "#E9190F";
+          } else if (
+            ageDate.valueAsDate > measurementDate.valueAsDate ||
+            ageDate.valueAsDate < measurementDate.valueAsDate
+          ) {
+            resultsDescription +=
+              "Data urodzenia powinna być wcześniejsza niż data pomiaru \r\n oraz dziecko powinno być w wieku 2 - 19 lat.";
+            ageDate.style.border = "2px solid #E9190F";
+            ageSubtitle.style.color = "#E9190F";
+          }
+        }
+
+        resultsDiv.textContent = resultsDescription;
+        resultsDiv.innerHTML = resultsDiv.innerHTML.replace(/\r\n?/g, "<br />");
+        resultsDiv.style.opacity = 1;
+      });
+    }
+  });
+});
+
+// default values for first display form
+setInitialValues(
+  "heightAdult",
+  "weightAdult",
+  ".height__output--adult",
+  ".btn-adult",
+  ".decreaseWeightAdult",
+  ".increaseWeightAdult",
+  "measureWeightAdult"
+);
 
 submit.addEventListener("click", (e) => {
   e.preventDefault();
-
-  const weightMeasure = document.getElementById("measureWeight").value;
-
+  let resultsDescription = "";
   const weight =
-    weightMeasure === "kg" ? +inputWeight.value : inputWeight.value * lb;
-  let descriptionBMI = "";
+    weightMeasure.value === "kg"
+      ? +weightValue.value
+      : lbOnKg(+weightValue.value);
 
-  if (
-    inputWeight.value > 0 &&
-    inputAge.value > 0 &&
-    (male.checked || female.checked)
-  ) {
-    const BMI = (weight / (inputHeight.value / 100) ** 2).toFixed(2);
+  weightValue.style.border = "1px solid transparent";
+  document.querySelector(".weight__subtitle").style.color = "#fff";
 
-    if (BMI <= 18.5) {
-      descriptionBMI = "niedowaga";
-    } else if (BMI <= 24.9) {
-      descriptionBMI = "prawidłowa masa ciała";
-    } else if (BMI <= 29.9) {
-      descriptionBMI = "nadwaga";
-    } else if (BMI <= 34.9) {
-      descriptionBMI = "otyłość I stopnia";
-    } else if (BMI <= 39.9) {
-      descriptionBMI = "otyłość II stopnia";
-    } else if (BMI >= 40) {
-      descriptionBMI = "otyłość III stopnia (otyłość skrajna)";
+  if (+weightValue.value > 0) {
+    const resultBMI = calcBMI(weight, +heightValue.value);
+    resultsDescription = `Twój wskaźnik BMI wynosi: ${resultBMI.description} ${
+      resultBMI.BMI
+    }\r\nTwój wzrost wynosi ${+heightValue.value}\r\n Twoja waga ${
+      weightMeasure.value === "kg"
+        ? +weightValue.value + " kg"
+        : +weightValue.value + " lb"
+    }`;
+  } else {
+    if (+weightValue.value <= 0) {
+      resultsDescription +=
+        "Uzupełnij formularz! \r\n Waga nie może być równa/mniejsza 0. \r\n";
+      weightValue.style.border = "2px solid #E9190F";
+      document.querySelector(".weight__subtitle").style.color = "#E9190F";
     }
-    // const bmiDescriptions = {
-    //   18.5: "niedowaga",
-    //   24.9: "prawidłowa masa ciała",
-    //   29.9: "nadwaga",
-    //   34.9: "otyłość I stopnia",
-    //   39.9: "otyłość II stopnia",
-    //   Infinity: "otyłość III stopnia (otyłość skrajna)",
-    // };
-
-    // const descriptionBMI = Object.entries(bmiDescriptions).find(
-    //   ([bmiValue, description]) => BMI <= bmiValue
-    // )[1];
-
-    document.querySelector(".results__bmi").textContent = BMI;
-    document.querySelector(".results__bmi--description").textContent =
-      descriptionBMI;
-    document.querySelector(".results__weight").textContent =
-      weight.toFixed(2) + ` kg`;
-    document.querySelector(".results__height").textContent = inputHeight.value;
-    document.querySelector(".results__age").textContent = inputAge.value;
-    document.querySelector(".results__gender").textContent = male.checked
-      ? "mężczyzna"
-      : "kobieta";
   }
-  //validation input gender
-  if (!male.checked && !female.checked) {
-    if (!document.querySelector(".gender .subtitle .empty")) {
-      const x = document.querySelector(".gender .subtitle");
-      x.insertAdjacentHTML(
-        "beforeend",
-        '<span class="empty emptyGender">Uzupełnij pole!</span>'
-      );
-    }
 
-    // inputWeight.style.border = "1px solid #d36135";
-  } else if (document.querySelector(".emptyGender")) {
-    document.querySelector(".emptyGender").remove();
-  }
-  //validation input age
-
-  if (inputAge.value == 0) {
-    if (!document.querySelector(".age .subtitle .emptyAge")) {
-      const x = document.querySelector(".age .subtitle");
-      x.insertAdjacentHTML(
-        "beforeend",
-        '<span class="empty emptyAge">Uzupełnij pole!</span>'
-      );
-      inputAge.style.border = "1px solid #d36135";
-    }
-  } else if (document.querySelector(".emptyAge")) {
-    document.querySelector(".emptyAge").remove();
-    inputAge.style.border = "1px solid transparent";
-  }
-  //validation input weight
-
-  //rozwiązanie ghatgpt
-  const emptyWeight = document.querySelector(".emptyWeight");
-  inputWeight.value == 0
-    ? !emptyWeight &&
-      document
-        .querySelector(".weight .subtitle")
-        .insertAdjacentHTML(
-          "beforeend",
-          '<span class="empty emptyWeight">Uzupełnij pole!</span>'
-        )
-    : emptyWeight?.remove();
-  // if (inputWeight.value == 0) {
-  //   if (!document.querySelector(".weight .subtitle .emptyWeight")) {
-  //     const x = document.querySelector(".weight .subtitle");
-  //     x.insertAdjacentHTML(
-  //       "beforeend",
-  //       '<span class="empty emptyWeight">Uzupełnij pole!</span>'
-  //     );
-  //     inputWeight.style.border = "1px solid #d36135";
-  //   }
-  // } else if (document.querySelector(".emptyWeight")) {
-  //   document.querySelector(".emptyWeight").remove();
-  //   inputWeight.style.border = "1px solid transparent";
-  // }
+  resultsDiv.textContent = resultsDescription;
+  resultsDiv.innerHTML = resultsDiv.innerHTML.replace(/\r\n?/g, "<br />");
+  resultsDiv.style.opacity = 1;
 });
+//create result div
+const resultsDiv = document.createElement("div");
+resultsDiv.className = "results";
+//append results div#calculator
+const calculatorContainer = document.getElementById("calculator");
+calculatorContainer.appendChild(resultsDiv);
